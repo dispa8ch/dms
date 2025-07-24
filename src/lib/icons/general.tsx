23f7147c9ea7ef904af0accc237f43b689b1f2fa
KingTony Technologies <kingtony3825@gmail.com>
2025-07-24 15:32:48 +0100

    worked on the register page

--- src/lib/icons/general.tsx
+++ src/lib/icons/general.tsx
@@ -730,4 +730,18 @@ export const GeneralIcons = {
       </defs>
     </svg>
   ),
+  select_drop: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="#757575"
+        d="M11.29 10.21a1 1 0 0 0 1.42 0l3-3a1.004 1.004 0 1 0-1.42-1.42L12 8.09l-2.29-2.3a1.004 1.004 0 0 0-1.42 1.42zm1.42 4.58a1 1 0 0 0-1.42 0l-3 3a1.004 1.004 0 1 0 1.42 1.42l2.29-2.3 2.29 2.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42z"
+      ></path>
+    </svg>
+  ),
 };
--- src/pages/auth/register/step_two.tsx
+++ src/pages/auth/register/step_two.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "@/pages/auth/style/Index.module.css";
 import Dispa8chInput from "@/lib/inputs/Dispa8chInput";
 import { useRegister } from "@/contexts/RegisterContext";
+import Dispa8chSelect from "@/lib/inputs/Dispa8chSelect";
+import { getCountries } from "@yusifaliyevpro/countries";
 
 function StepTwo() {
   const business = useRegister();
+  const [countries, setCountries] = React.useState<Dispa8chSelectOption[]>([]);
+  const [loading, setLoading] = React.useState(false);
+  useEffect(() => {
+    const fetchCountries = async () => {
+      setLoading(true);
+      // Fetch all countries with specific fields
+      const countries = await getCountries({
+        fields: ["name", "flags"],
+      });
+      if (countries && Array.isArray(countries)) {
+        console.log(countries);
+
+        setCountries(
+          countries.map((country) => {
+            return {
+              label: country.name.common,
+              value: country.name.common.split(" ").join("-").toLowerCase(),
+              extra: country.flags.png,
+            };
+          })
+        );
+      }
+      setLoading(false);
+    };
+    fetchCountries();
+  }, []);
   return (
     <div>
       <div className={styles.input_wrapper}>
@@ -17,6 +45,34 @@ function StepTwo() {
             business?.setRegisterPayload((prev) => ({
               ...prev,
               companyName: value as string,
+            }));
+          }}
+        />
+      </div>
+      <div className={styles.input_wrapper}>
+        <Dispa8chInput
+          label="Business email"
+          type="email"
+          placeholder="company@example.com"
+          value={business?.business_email}
+          onChange={(value) => {
+            business?.setRegisterPayload((prev) => ({
+              ...prev,
+              business_email: value as string,
+            }));
+          }}
+        />
+      </div>
+      <div className={styles.input_wrapper}>
+        <Dispa8chSelect
+          options={countries}
+          label="Country"
+          placeholder="Select country"
+          value={business?.country}
+          onChange={(value) => {
+            business?.setRegisterPayload((prev) => ({
+              ...prev,
+              country: value.label as string,
             }));
           }}
         />
@@ -37,9 +93,23 @@ function StepTwo() {
       </div>
       <div className={styles.input_wrapper}>
         <Dispa8chInput
+          label="Business address"
+          type="text"
+          placeholder="123 Ave. Abc County"
+          value={business?.address}
+          onChange={(value) => {
+            business?.setRegisterPayload((prev) => ({
+              ...prev,
+              address: value as string,
+            }));
+          }}
+        />
+      </div>
+      <div className={styles.input_wrapper}>
+        <Dispa8chInput
           label="Phone number"
           type="phone"
-          // placeholder="example@gmail.com"
+          placeholder="9063213825"
           value={business?.phone}
           onChange={(value) => {
             business?.setRegisterPayload((prev) => ({
@@ -49,20 +119,6 @@ function StepTwo() {
           }}
         />
       </div>
-      {/* <div className={styles.input_wrapper}>
-        <Dispa8chInput
-          label="Password"
-          type="password"
-          value={business?.user.user_password}
-          placeholder="enter your password"
-          onChange={(value) => {
-            business?.setRegisterPayload((prev) => ({
-              ...prev,
-              user: { ...prev.user, user_password: value as string },
-            }));
-          }}
-        />
-      </div> */}
     </div>
   );
 }
